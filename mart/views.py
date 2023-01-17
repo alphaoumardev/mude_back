2d4c0f307f5e660ef59066d14003fe11f7bd9b60
alphaoumardev <alphaoumardev@outlook.com>
2023-01-17 23:00:45 +0800

    jenny

--- mart/views.py
+++ mart/views.py
@@ -16,14 +16,14 @@ from mart.serializers import ProductSerializer, ColorsOptionSerializer, TagSeria
 """To Jenny"""
 
 
-# print('\n'.join
-#       ([''.join
-#         ([('Jenny'[(x - y) % 5]
-#            if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1)
-#               ** 3 - (x * 0.05) ** 2 * (y * 0.1)
-#               ** 3 <= 0 else ' ')
-#           for x in range(-30, 30)])
-#         for y in range(15, -15, -1)]))
+print('\n'.join
+      ([''.join
+        ([('Jenny'[(x - y) % 5]
+           if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1)
+              ** 3 - (x * 0.05) ** 2 * (y * 0.1)
+              ** 3 <= 0 else ' ')
+          for x in range(-30, 30)])
+        for y in range(15, -15, -1)]))
 
 
 class MyPageNumberPagination(PageNumberPagination):
