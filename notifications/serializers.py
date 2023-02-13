a8abc7af771e37627c9b4053fc414ede71e31a51
alphaoumardev <alphaoumardev@outlook.com>
2023-02-13 19:28:56 +0800

    adding products

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,15 +1,15 @@
-# from rest_framework import serializers
-# from notifications.models import Notifications
-#
-#
-# class NotificationSerializer(serializers.ModelSerializer):
-#     # from_profile = serializers.SerializerMethodField(read_only=True, required=False)
-#
-#     class Meta:
-#         model = Notifications
-#         fields = '__all__'
-#         depth = 1
-#
-#     @staticmethod
-#     def get_from_profile(obj):
-#         return CommentatorSerializer(obj.from_profile, many=False).data
+from rest_framework import serializers
+from notifications.models import Notifications
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    # from_profile = serializers.SerializerMethodField(read_only=True, required=False)
+
+    class Meta:
+        model = Notifications
+        fields = '__all__'
+        depth = 1
+
+    @staticmethod
+    def get_from_profile(obj):
+        return CommentatorSerializer(obj.from_profile, many=False).data
